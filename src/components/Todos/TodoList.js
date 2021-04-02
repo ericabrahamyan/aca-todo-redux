8e4d76be0474234bbdfa5c0f01087cd23e5d1945
Eric <erabrahamyan@gmail.com>
2021-04-02 19:56:22 +0400

    refactored components use functional components instead of classes

--- src/components/Todos/TodoList.js
+++ src/components/Todos/TodoList.js
@@ -1,25 +1,22 @@
-import { Component } from "react";
 import TodoItem from "./TodoItem";
 
-class TodoList extends Component {
-  render() {
-    const { items, onEdit, onDelete, onToggleComplete } = this.props;
+const TodoList = (props) => {
+  const { items, onEdit, onDelete, onToggleComplete } = props;
 
-    if (!items.length) return <p>There is no items</p>;
-    return (
-      <>
-        {items.map((item) => (
-          <TodoItem 
-          key={item.id} 
-          item={item} 
+  if (!items.length) return <p>There is no items</p>;
+  return (
+    <>
+      {items.map((item) => (
+        <TodoItem
+          key={item.id}
+          item={item}
           onDelete={onDelete}
           onEdit={onEdit}
           onToggleComplete={onToggleComplete}
-          />
-        ))}
-      </>
-    );
-  }
-}
+        />
+      ))}
+    </>
+  );
+};
 
 export default TodoList;
